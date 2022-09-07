7488f296eb9460dd278d81e9e938d4714fdfc71e
Mauro Ruiz <mauroruiz146@gmail.com>
2022-09-07 15:06:08 -0500

    fisrt commit

---
+++ components/layouts/MainLayout.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import { Navbar } from '../Navbar';
+import  styles from './MainLayout.module.css';
+
+export const MainLayout = ({ children }) => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>My Next App</title>
+                <meta name="description" content="Next App" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        
+            <Navbar/>
+        
+            <main className={styles.main}>
+        
+                { children }
+                
+            </main>
+        
+            
+            </div>
+        )
+}
---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { LightLayout } from "../components/layouts/LightLayout";
+import { MainLayout } from "../components/layouts/MainLayout";
+
+export default function AboutPage() {
+  return (
+    <>
+        <h1>About Page</h1>
+
+        <h1 className={'title'}>
+          Ir a <Link href="/">Home</Link>
+        </h1>
+
+        <p className={'description'}>
+          Editar <code className={'code'}>pages/about.js</code>
+        </p>
+    </>
+  );
+}
+
+AboutPage.getLayout = function getLayout( page ) {
+  return (
+    <MainLayout>
+      <LightLayout>
+        { page }
+      </LightLayout>
+    </MainLayout>
+  )
+}
